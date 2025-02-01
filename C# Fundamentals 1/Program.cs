d9f35764ada723bb563cbcf7f46a5ec4b7d7df90
dxcso <dxcso@DESKTOP-0OFSMPT>
2025-02-01 02:07:41 -0600

    Add project files.

---
+++ C# Fundamentals 1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace C__Fundamentals_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            OutputStrings();
+            Arithmetic();
+            Assignment();
+            ComparisonAndConditionals();
+        }
+
+        private void OutputStrings()
+        {
+            string sentence1 = "I'm in Game 1377: Scripting for Game Designers";
+        }
+        private void Arithmetic()
+        {
+
+        }
+        private void Assignment()
+        {
+
+        }
+        private void ComparisonAndConditionals()
+        {
+
+        }
+    }
+}
